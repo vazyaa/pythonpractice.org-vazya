8fd16203447307fdbc51f7536a3935df3541ca94
Vazya <101694082+vazyaa@users.noreply.github.com>
2025-07-30 03:26:58 +0200

    Add files via upload

---
+++ exercise1.py
@@ -0,0 +1,8 @@
+name = input("Please input your name: ")
+age = int(input("Please input your age: "))
+yearOfBirth = 2025 - age
+year100 = yearOfBirth + 100
+print("Your name is " + name + " and you will turn 100 in " + str(year100) + ".")
+please = "Please input a number: "
+number = input(please)
+print("Please input a number: \n" * int(number))
